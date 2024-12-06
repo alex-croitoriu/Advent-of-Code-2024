239f618949cdaf29edc496c3c05a27b49e795545
Alex Croitoriu <alexcroitoriu2005@gmail.com>
2024-12-06 12:10:58 +0200

    day03

--- days/day03/src/part1.rs
+++ days/day03/src/part1.rs
@@ -1,16 +1,21 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use regex::Regex;
 
 const PKG_NAME: &str = env!("CARGO_PKG_NAME");
 
 pub fn solve() {
     let mut ans = 0;
+    let re = Regex::new(r"mul\([0-9]+,[0-9]+\)").unwrap();
 
     if let Ok(file) = File::open(format!("inputs/{PKG_NAME}.txt")) {
         let lines: Vec<String> = BufReader::new(file).lines().map(|l| l.unwrap()).collect();
         
         for line in lines {
-            
+            let matches = re.find_iter(line.as_str());
+            for mat in matches {
+                ans += mat.as_str().split('(').nth(1).unwrap().split(')').nth(0).unwrap().split(',').fold(1, |acc, x: &str| acc * x.parse::<i32>().unwrap());
+            }
         }
     }
 
--- days/day03/src/part2.rs
+++ days/day03/src/part2.rs
@@ -1,16 +1,30 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use regex::Regex;
 
 const PKG_NAME: &str = env!("CARGO_PKG_NAME");
 
 pub fn solve() {
     let mut ans = 0;
+    let re = Regex::new(r"mul\([0-9]+,[0-9]+\)|do\(\)|don\'t\(\)").unwrap();
+    let mut enabled = true;
 
     if let Ok(file) = File::open(format!("inputs/{PKG_NAME}.txt")) {
         let lines: Vec<String> = BufReader::new(file).lines().map(|l| l.unwrap()).collect();
         
         for line in lines {
-            
+            let matches = re.find_iter(line.as_str());
+            for mat in matches {
+                if mat.as_str() == "do()" {
+                    enabled = true;
+                }
+                else if mat.as_str() == "don\'t()" {
+                    enabled = false;
+                }
+                else if enabled {
+                    ans += mat.as_str().split('(').nth(1).unwrap().split(')').nth(0).unwrap().split(',').fold(1, |acc, x: &str| acc * x.parse::<i32>().unwrap());
+                }
+            }
         }
     }
 
