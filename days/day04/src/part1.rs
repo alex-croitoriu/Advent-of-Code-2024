285eb378f8621135ab9df818e2e5c643c4ba273e
Alex Croitoriu <alexcroitoriu2005@gmail.com>
2024-12-06 14:04:12 +0200

    day04

--- days/day04/src/part1.rs
+++ days/day04/src/part1.rs
@@ -1,16 +1,99 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use regex::Regex;
 
 const PKG_NAME: &str = env!("CARGO_PKG_NAME");
+
+pub fn matches(s: &String) -> i32 {
+    let mut ans = 0;
+    let re = vec![Regex::new(r"XMAS").unwrap(), Regex::new(r"SAMX").unwrap()];
+
+    for r in re {
+        ans += r.captures_iter(s.as_str()).collect::<Vec<_>>().len() as i32;
+    }
+
+    ans
+}
 
 pub fn solve() {
     let mut ans = 0;
 
+    let mut table: Vec<Vec<char>> = Vec::new();
+
     if let Ok(file) = File::open(format!("inputs/{PKG_NAME}.txt")) {
         let lines: Vec<String> = BufReader::new(file).lines().map(|l| l.unwrap()).collect();
+
+        let n = lines.len();
+        let m = lines[0].len();
         
         for line in lines {
-            
+            table.push(line.chars().collect::<Vec<char>>());
+        }
+
+        for line in &table {
+            ans += matches(&line.iter().collect::<String>());
+        }
+
+        for i in 0..m {
+            let mut column: String = String::from("");
+
+            for j in 0..n {
+                column.push(table[j][i]);
+            }
+
+            ans += matches(&column);
+        }
+
+        for i in 0..m {
+            let (mut x, mut y) = (0, i);
+            let mut diagonal: String = String::from("");
+
+            while x < n && y < m {
+                diagonal.push(table[x][y]);
+                x += 1;
+                y += 1;
+            }
+
+            ans += matches(&diagonal);
+        }
+
+        for i in 1..n {
+            let (mut x, mut y) = (i, 0);
+            let mut diagonal: String = String::from("");
+
+            while x < n && y < m {
+                diagonal.push(table[x][y]);
+                x += 1;
+                y += 1;
+            }
+
+            ans += matches(&diagonal); 
+        }
+
+        for i in 0..m {
+            let (mut x, mut y) = (0, i as isize);
+            let mut diagonal: String = String::from("");
+
+            while x < n && y >= 0 {
+                diagonal.push(table[x][y as usize]);
+                x += 1;
+                y -= 1;
+            }
+
+            ans += matches(&diagonal);
+        }
+
+        for i in 1..n {
+            let (mut x, mut y) = (i, (m - 1) as isize);
+            let mut diagonal: String = String::from("");
+
+            while x < n && y >= 0 {
+                diagonal.push(table[x][y as usize]);
+                x += 1;
+                y -= 1;
+            }
+
+            ans += matches(&diagonal);
         }
     }
 
--- days/day04/src/part2.rs
+++ days/day04/src/part2.rs
@@ -3,14 +3,43 @@ use std::io::{BufRead, BufReader};
 
 const PKG_NAME: &str = env!("CARGO_PKG_NAME");
 
+pub fn is_valid(v: Vec<Vec<char>>) -> bool {
+    ((v[0][0] == 'M' && v[2][2] == 'S') || (v[0][0] == 'S' && v[2][2] == 'M')) &&
+    ((v[2][0] == 'M' && v[0][2] == 'S') || (v[2][0] == 'S' && v[0][2] == 'M')) && 
+    v[1][1] == 'A'
+}
+
 pub fn solve() {
     let mut ans = 0;
 
+    let mut table: Vec<Vec<char>> = Vec::new();
+
     if let Ok(file) = File::open(format!("inputs/{PKG_NAME}.txt")) {
         let lines: Vec<String> = BufReader::new(file).lines().map(|l| l.unwrap()).collect();
+
+        let n = lines.len();
+        let m = lines[0].len();
         
         for line in lines {
-            
+            table.push(line.chars().collect::<Vec<char>>());
+        }
+
+        for i in 0..n - 2 {
+            for j in 0..m - 2 {
+                let mut v: Vec<Vec<char>> = Vec::new();
+                
+                for k in 0..3 {
+                    v.push(vec![
+                        table[i + k][j],
+                        table[i + k][j + 1],
+                        table[i + k][j + 2]
+                    ]);
+                }
+
+                if is_valid(v) {
+                    ans += 1;
+                }
+            }
         }
     }
 
